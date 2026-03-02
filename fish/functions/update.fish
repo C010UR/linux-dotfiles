b8e01c5752c15b65e81f4d529662a8d3fcc95f03
Mikhail Buinouskiy <buinouskiymikhail@gmail.com>
2026-03-02 17:57:12 +0300

    Update stuff

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -5,14 +5,19 @@ function update
     yay -Syu --noconfirm --answerclean None --answerdiff None --answeredit None
 
     set -l to_rebuild
-    set -a to_rebuild (pacman -Slq caelestia 2>/dev/null)
+
+    for pkg in (pacman -Slq caelestia 2>/dev/null)
+        test -n "$pkg"; and set -a to_rebuild $pkg
+    end
 
     if test "$py_ver" != (pacman -Q python 2>/dev/null)
         echo "Python updated — queuing all Python package rebuilds…"
-        set -a to_rebuild (yay -Qoq $py_dir 2>/dev/null)
+        for pkg in (yay -Qoq $py_dir 2>/dev/null)
+            test -n "$pkg"; and set -a to_rebuild $pkg
+        end
     end
 
-    set to_rebuild (printf '%s\n' $to_rebuild | sort -u)
+    set to_rebuild (printf '%s\n' $to_rebuild | string match -rv '^\s*$' | sort -u)
 
     if test (count $to_rebuild) -gt 0
         yay -S $to_rebuild --noconfirm --answerclean All --answerdiff None --answeredit None
