51c4c3c8d820c497055881ac3d0a5dac0e0cb7db
Mikhail Buinouskiy <buinouskiymikhail@gmail.com>
2026-03-09 01:13:14 +0300

    Update stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-source $__fish_config_dir/conf.d/environment.fish
-
 if status is-interactive
     # Starship custom prompt
     starship init fish | source
@@ -7,9 +5,6 @@ if status is-interactive
     # Direnv + Zoxide
     command -v direnv &> /dev/null && direnv hook fish | source
     command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
-
-    # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
-
-    a
 end
+
+# cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
