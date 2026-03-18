cdc9af5f4ccf291b52ae04a45ff3288a3abe2a37
Mikhail Buinouskiy <buinouskiymikhail@gmail.com>
2026-03-18 12:15:45 +0200

    Project cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,6 @@ if status is-interactive
     # Direnv + Zoxide
     command -v direnv &> /dev/null && direnv hook fish | source
     command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+    command -v rbenv &> /dev/null && rbenv init - fish | source
 end
-
 # cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
